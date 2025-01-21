8508d3e5d1ba8d1c06cc57d6ec92bb0ef73e3ae1
Lukas Petz <lukas.petz@campus.lmu.de>
2025-01-21 08:50:17 -0800

    updates_lecture

---
+++ notes.md
@@ -0,0 +1,7 @@
+**Session 21.01**
+- kitchen repo contains several example nodes
+- clone relevant nodes as subrepo with naming scheme group-name_node-name
+
+***Usage***
+- rosrun *.py instead of python *.py
+- `roslaunch mycobot_280_gripper_moveit mycobot_moveit.launch` aswell as `roscore` need to run in background 
---
+++ src/test/scripts/reset_to_zero.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import rospy
+import time
+from pymycobot import MyCobot
+
+def reset_to_zero(mc):
+    # Gets the current angle of all joints
+    angles = mc.get_angles()
+    print(angles)
+
+    for joint_id in range(1, 7):
+        mc.send_angle(joint_id, 0, 50)
+        time.sleep(1)
+    angles = mc.get_angles()
+    print(angles)
+
+if __name__ == "__main__":
+    rospy.init_node("mycobot_test", anonymous=True)
+    port = rospy.get_param("~port", "/dev/ttyACM0")
+    baud = rospy.get_param("~baud", 115200)
+    print(port, baud)
+    mc = MyCobot(port, baud)
+
+    reset_to_zero(mc)
